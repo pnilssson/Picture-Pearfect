34b679ff2dc45da8d5750e0a3727aacb57a1b02e
Pär Nilsson <43166371+pnilssson@users.noreply.github.com>
2019-03-13 12:03:00 +0100

    Picture Pearfect v1.0

---
+++ Projektarbete/js/cardArrays.js
@@ -0,0 +1,245 @@
+//FRUKTER
+const fruitArray = [{
+    'name': 'orange',
+    'img': 'img/fruktbilder/orange.png',
+    },
+    {
+        'name': 'orange',
+        'img': 'img/fruktbilder/apelsin-text.png',
+    },
+    {
+        'name': 'ananas',
+        'img': 'img/fruktbilder/pineapple.png',
+    },
+    {
+        'name': 'ananas',
+        'img': 'img/fruktbilder/ananas-text.png',
+    },
+    {
+        'name': 'apple',
+        'img': 'img/fruktbilder/apple.png',
+    },
+    {
+        'name': 'apple',
+        'img': 'img/fruktbilder/äpple-text.png',
+    },
+    {
+        'name': 'apricot',
+        'img': 'img/fruktbilder/apricot.png',
+    },
+    {
+        'name': 'apricot',
+        'img': 'img/fruktbilder/aprikos-text.png',
+    },
+    {
+        'name': 'banana',
+        'img': 'img/fruktbilder/banana.png',
+    },
+    {
+        'name': 'banana',
+        'img': 'img/fruktbilder/banan-text.png',
+    },
+    {
+        'name': 'clementine',
+        'img': 'img/fruktbilder/clementine.png',
+    },
+    {
+        'name': 'clementine',
+        'img': 'img/fruktbilder/clementin-text.png',
+    },
+    {
+        'name': 'kiwi',
+        'img': 'img/fruktbilder/kiwi.png',
+    },
+    {
+        'name': 'kiwi',
+        'img': 'img/fruktbilder/kiwi-text.png',
+    },
+    {
+        'name': 'lime',
+        'img': 'img/fruktbilder/lime.png',
+    },
+    {
+        'name': 'lime',
+        'img': 'img/fruktbilder/lime-text.png',
+    },
+    {
+        'name': 'plums',
+        'img': 'img/fruktbilder/plums.png',
+    },
+    {
+        'name': 'plums',
+        'img': 'img/fruktbilder/plommon-text.png',
+    },
+    {
+        'name': 'watermelon',
+        'img': 'img/fruktbilder/watermelon.png',
+    },
+    {
+        'name': 'watermelon',
+        'img': 'img/fruktbilder/vattenmelon-text.png',
+    },
+];
+
+
+//DJUR
+const animalArray = [{
+    'name': 'cat',
+    'img': 'img/djurbilder/cat.png',},
+    {
+        'name': 'chicken',
+        'img': 'img/djurbilder/chicken.png',
+    },
+    {
+        'name': 'cow',
+        'img': 'img/djurbilder/cow.png',
+    },
+    {
+        'name': 'dog',
+        'img': 'img/djurbilder/dog.png',
+    },
+    {
+        'name': 'donkey',
+        'img': 'img/djurbilder/donkey.png',
+    },
+    {
+        'name': 'goat',
+        'img': 'img/djurbilder/goat.png',
+    },
+    {
+        'name': 'horse',
+        'img': 'img/djurbilder/horse.png',
+    },
+    {
+        'name': 'pig',
+        'img': 'img/djurbilder/pig.png',
+    },
+    {
+        'name': 'sheep',
+        'img': 'img/djurbilder/sheep.png',
+    },
+    {
+        'name': 'rabbit',
+        'img': 'img/djurbilder/rabbit.png',
+    },
+    {
+        'name': 'cat',
+        'img': 'img/djurbilder/cat-text.png',},
+    {
+        'name': 'chicken',
+        'img': 'img/djurbilder/chicken-text.png',
+    },
+    {
+        'name': 'cow',
+        'img': 'img/djurbilder/cow-text.png',
+    },
+    {
+        'name': 'dog',
+        'img': 'img/djurbilder/dog-text.png',
+    },
+    {
+        'name': 'donkey',
+        'img': 'img/djurbilder/donkey-text.png',
+    },
+    {
+        'name': 'goat',
+        'img': 'img/djurbilder/goat-text.png',
+    },
+    {
+        'name': 'horse',
+        'img': 'img/djurbilder/horse-text.png',
+    },
+    {
+        'name': 'pig',
+        'img': 'img/djurbilder/pig-text.png',
+    },
+    {
+        'name': 'sheep',
+        'img': 'img/djurbilder/sheep-text.png',
+    },
+    {
+        'name': 'rabbit',
+        'img': 'img/djurbilder/rabbit-text.png',
+    },
+];
+
+//SIFFROR
+const numbersArray = [{
+    'name': 'zero',
+    'img': 'img/siffrorbilder/zero.png',},
+    {
+        'name': 'one',
+        'img': 'img/siffrorbilder/one.png',
+    },
+    {
+        'name': 'two',
+        'img': 'img/siffrorbilder/two.png',
+    },
+    {
+        'name': 'three',
+        'img': 'img/siffrorbilder/three.png',
+    },
+    {
+        'name': 'four',
+        'img': 'img/siffrorbilder/four.png',
+    },
+    {
+        'name': 'five',
+        'img': 'img/siffrorbilder/five.png',
+    },
+    {
+        'name': 'six',
+        'img': 'img/siffrorbilder/six.png',
+    },
+    {
+        'name': 'seven',
+        'img': 'img/siffrorbilder/seven.png',
+    },
+    {
+        'name': 'eight',
+        'img': 'img/siffrorbilder/eight.png',
+    },
+    {
+        'name': 'nine',
+        'img': 'img/siffrorbilder/nine.png',
+    },
+    {
+        'name': 'zero',
+        'img': 'img/siffrorbilder/zero-text.png',},
+    {
+        'name': 'one',
+        'img': 'img/siffrorbilder/one-text.png',
+    },
+    {
+        'name': 'two',
+        'img': 'img/siffrorbilder/two-text.png',
+    },
+    {
+        'name': 'three',
+        'img': 'img/siffrorbilder/three-text.png',
+    },
+    {
+        'name': 'four',
+        'img': 'img/siffrorbilder/four-text.png',
+    },
+    {
+        'name': 'five',
+        'img': 'img/siffrorbilder/five-text.png',
+    },
+    {
+        'name': 'six',
+        'img': 'img/siffrorbilder/six-text.png',
+    },
+    {
+        'name': 'seven',
+        'img': 'img/siffrorbilder/seven-text.png',
+    },
+    {
+        'name': 'eight',
+        'img': 'img/siffrorbilder/eight-text.png',
+    },
+    {
+        'name': 'nine',
+        'img': 'img/siffrorbilder/nine-text.png',
+    },
+];
